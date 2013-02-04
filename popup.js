ff2e395fb856cc3c7520acde7e299f3eff154322
Mark Bell <me@markashleybell.com>
2013-02-04 15:15:45 +0000

    Temporary fix for bizarre XHR bug

--- popup.js
+++ popup.js
@@ -8,28 +8,35 @@ function onPageInfo(o)  {
 
 // POST the data to the server using XMLHttpRequest
 function addBookmark() {
-    var postUrl = "http://yourwebappurl/addbookmark/";
-    var req = new XMLHttpRequest();
-    req.open("POST", postUrl, true);
+    var postUrl = "http://appurl/addbookmark";
+    var xhr = new XMLHttpRequest();
+
+    // This async request used to work, but now doesn't for some reason...
+    // It works if you 'Inspect popup' and step through the code in the Chrome debugger,
+    // but as soon as you close the debugger and try the same thing, the call fails?!??
+
+    // xhr.onreadystatechange = function() { 
+    //     // If the request completed, close the extension popup
+    //     if (this.readyState == 4) {
+    //         if (this.status == 200) 
+    //             window.close();
+    //     }
+    // };
+
+    // xhr.open("POST", postUrl, true);
+
+    // Non-asynchronous request works fine
+    xhr.open("POST", postUrl, false);
     
     var params = "title=" + document.getElementById("title").value + 
                  "&url=" + document.getElementById("url").value + 
                  "&summary=" + document.getElementById("summary").value +
                  "&tags=" + document.getElementById("tags").value;
     
-    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    req.send(params);
-    
-    req.onreadystatechange = function() 
-    { 
-        // If the request completed, close the extension popup
-        if (req.readyState == 4)
-            if (req.status == 200) 
-                window.close();
-            else
-                alert("Could not reach server " + postUrl);
-        
-    };
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+
+    window.close();
 
     return false;
 }
