1039ff85021e8654e5f5a9384ff9c8916fa873e9
Mark Bell <me@markb.co.uk>
2018-08-18 13:07:03 +0100

    Simplify code, replace old permissions with activeTab

--- popup.js
+++ popup.js
@@ -1,45 +1,31 @@
-// This callback function is called when the content script has been 
-// injected and returned its results
-function onPageDetailsReceived(pageDetails)  { 
-    document.getElementById('title').value = pageDetails.title; 
-    document.getElementById('url').value = pageDetails.url; 
-    document.getElementById('summary').innerText = pageDetails.summary; 
-} 
+// The URL to POST our data to
+const postUrl = 'http://post-test.local.com';
 
 // Global reference to the status display SPAN
-var statusDisplay = null;
+let statusDisplay = null;
 
 // POST the data to the server using XMLHttpRequest
-function addBookmark() {
+function addBookmark(e) {
     // Cancel the form submit
-    event.preventDefault();
-
-    // The URL to POST our data to
-    var postUrl = 'http://post-test.local.com';
-
+    e.preventDefault();
+    
     // Set up an asynchronous AJAX POST request
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('POST', postUrl, true);
     
     // Prepare the data to be POSTed by URLEncoding each field's contents
-    var title = encodeURIComponent(document.getElementById('title').value);
-    var url = encodeURIComponent(document.getElementById('url').value);
-    var summary = encodeURIComponent(document.getElementById('summary').value);
-    var tags = encodeURIComponent(document.getElementById('tags').value);
+    const title = encodeURIComponent(document.getElementById('title').value);
+    const url = encodeURIComponent(document.getElementById('url').value);
+    const summary = encodeURIComponent(document.getElementById('summary').value);
+    const tags = encodeURIComponent(document.getElementById('tags').value);
     
-    var params = 'title=' + title + 
-                 '&url=' + url + 
-                 '&summary=' + summary +
-                 '&tags=' + tags;
-    
-    // Replace any instances of the URLEncoded space char with +
-    params = params.replace(/%20/g, '+');
+    const parameters = `title=${title}&url=${url}&summary=${summary}&tags=${tags}`;
 
     // Set correct header for form data 
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     // Handle request state change events
-    xhr.onreadystatechange = function() { 
+    xhr.onreadystatechange = () => { 
         // If the request completed
         if (xhr.readyState == 4) {
             statusDisplay.innerHTML = '';
@@ -49,27 +35,29 @@ function addBookmark() {
                 window.setTimeout(window.close, 1000);
             } else {
                 // Show what went wrong
-                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
+                const errorDetails = xhr.statusText || xhr.responseText || `no response from ${postUrl}`;
+                statusDisplay.innerHTML = `Error: ${errorDetails}`;
             }
         }
     };
 
     // Send the request and set status
-    xhr.send(params);
+    xhr.send(parameters);
     statusDisplay.innerHTML = 'Saving...';
 }
 
 // When the popup HTML has loaded
-window.addEventListener('load', function(evt) {
+window.addEventListener('load', e => {
     // Cache a reference to the status display SPAN
     statusDisplay = document.getElementById('status-display');
     // Handle the bookmark form submit event with our addBookmark function
     document.getElementById('addbookmark').addEventListener('submit', addBookmark);
-    // Get the event page
-    chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in 
-        // our onPageDetailsReceived function as the callback. This injects 
-        // content.js into the current tab's HTML
-        eventPage.getPageDetails(onPageDetailsReceived);
-    });
+    // Listen for a message from the content script we're about to inject
+    chrome.runtime.onMessage.addListener(message => { 
+        document.getElementById('title').value = message.title; 
+        document.getElementById('url').value = message.url; 
+        document.getElementById('summary').innerText = message.summary; 
+    }); 
+    // Inject the content script that gets the page details
+    chrome.tabs.executeScript({file: 'pagedetails.js'});
 });
